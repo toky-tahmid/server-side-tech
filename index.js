4461ca6a04c715a36ea1d458cd5882a301a6ac66
Your Name <you@example.com>
2023-10-18 23:43:13 +0600

    finding function added

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
 
     const productCollection = client.db('productDB').collection('product');
 
+app.get('/product',async (req, res) => {
+    const cursor = productCollection.find()
+    const result = await cursor.toArray();
+    res.send(result);
+})
+
 app.post('/product',async  (req, res)=>{
     const newProduct = req.body;
     console.log(newProduct);
