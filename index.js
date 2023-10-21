0258802dc2398a7ca8f038b7de3beda7000d53fb
Your Name <you@example.com>
2023-10-21 19:23:02 +0600

    full done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db("productDB").collection("product");
     const addCart = client.db("productDB").collection("cart");
@@ -40,6 +40,7 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
     app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
@@ -90,7 +91,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
